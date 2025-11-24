b100f050fdbdf2dffc1a168e1a0fe47745093780
nvathu-ntust <nvathu.dut@gmail.com>
2025-11-24 21:41:59 +0700

    add home page

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+// Set view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Serve static files
+app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
+
+// Route
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
